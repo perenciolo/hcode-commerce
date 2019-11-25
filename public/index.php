26236c2449fd3ac33d9be36fb65fa56a89ffa750
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2019-11-24 21:48:22 -0300

    Remove vendor from git

--- public/index.php
+++ public/index.php
@@ -1,1 +1,17 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function(){
+  $sql = new Perenciolo\DB\Sql();
+
+  $results = $sql->select("SELECT * FROM tb_users");
+
+  echo json_encode($results);
+});
+
+$app->run();
